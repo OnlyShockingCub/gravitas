2a3b8cb463074fc25b3a36e54d1f637f7fcac7f2
OnlyShockingCub <onlyshockingcub@gmail.com>
2026-05-31 17:51:36 -0400

    added ball movement

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
-fn main() {
-    println!("Hello, world!");
+use macroquad::prelude::*;
+
+struct Body {
+    x: f32,
+    y: f32,
+    vx: f32,
+    vy: f32
+}
+
+#[macroquad::main("gravitas")]
+
+async fn main() {
+    let mut ball = Body {
+        x: 0.0,
+        y: 10.0,
+        vx: 1.0,
+        vy: 0.0,
+    };
+
+    loop {
+        let dt = 1.0;
+
+        for step in 0..10 {
+            ball.x += ball.vx * dt;
+            ball.y += ball.vy * dt;
+        }
+
+        clear_background(BLACK);
+
+        draw_circle(ball.x, ball.y, 20.0, RED);
+
+        next_frame().await;
+    }
 }
