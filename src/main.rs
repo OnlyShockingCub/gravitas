367e79611857d9d2390506b8ed73d1f0ff7e9027
OnlyShockingCub <onlyshockingcub@gmail.com>
2026-06-01 20:05:37 -0400

    added mutliple bodies/balls running together

--- src/main.rs
+++ src/main.rs
@@ -6,13 +6,22 @@ use macroquad::prelude::*;
 
 #[macroquad::main("gravitas")]
 async fn main() {
-    let mut ball = Body {
-        x: 300.0,
-        y: 100.0,
-        vx: 200.0,
-        vy: 0.0,
-        r: 20.0,
-    };
+    let mut balls = vec![
+        Body {
+            x: 300.0,
+            y: 100.0,
+            vx: 200.0,
+            vy: 0.0,
+            r: 20.0,
+        },
+        Body {
+            x: 400.0,
+            y: 100.0,
+            vx: 200.0,
+            vy: 0.0,
+            r: 20.0,
+        },
+    ];
 
     let gravity = 900.0;
     let restitution = 0.7;
@@ -20,21 +29,26 @@ async fn main() {
     loop {
         let dt = get_frame_time();
 
-        physics::step(
-            &mut ball, 
-            dt, 
-            gravity);
+        for ball in &mut balls {
+            physics::step(
+                ball,
+                dt,
+                gravity,
+            );
 
-        physics::handle_bounds(
-            &mut ball,
-            screen_width(),
-            screen_height(),
-            restitution,
-        );
+            physics::handle_bounds(
+                ball,
+                screen_width(),
+                screen_height(),
+                restitution,
+            );
+        }
 
         clear_background(BLACK);
 
-        draw_circle(ball.x, ball.y, ball.r, RED);
+        for ball in &balls {
+            draw_circle(ball.x, ball.y, ball.r, RED);
+        }
 
         next_frame().await;
     }
--- src/physics.rs
+++ src/physics.rs
@@ -42,3 +42,4 @@ pub fn handle_bounds(
         }
     }
 }
+
